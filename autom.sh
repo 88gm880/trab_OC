9fa44a27288e36b465cd8416651683ce0d6be96c
88gm880 <gu.mcorrea@gmail.com>
2018-11-11 00:10:08 -0200

    Add files via upload
    Corrigido o for

--- autom.sh
+++ autom.sh
@@ -5,10 +5,10 @@ sed -i "s/#define TAM.*/#define TAM $tam/" p2.c
 gcc p1.c -o p1
 gcc p2.c -o p2
 echo "$tam," >> testes.csv
-for i in 1 2 3 4 do
-/p1
+for i in 1 2 3 4; do
+./p1
 done
 echo "$tam," >> testes.csv
-for i in 1 2 3 4 do
-/p2
+for i in 1 2 3 4; do
+./p2
 done
